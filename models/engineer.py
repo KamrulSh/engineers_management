70ecea4d28efab02abee1c48aa90ff887be16486
Kamrul Islam <shahin.cuetcse@gmail.com>
2021-11-17 12:42:57 +0600

    added some fields

--- models/engineer.py
+++ models/engineer.py
@@ -5,5 +5,21 @@ class EngineerInformation(models.Model):
     _inherit = 'hr.employee'
     _description = 'Engineers Information'
 
+    engineer_id = fields.Integer(string="Engineer ID", tracking=True)
+    engineer_type = fields.Selection([
+        ('parttime', 'Part time'),
+        ('fulltime', 'Full time'),
+        ('contract', 'Contract')
+    ], default="fulltime", tracking=True)
+
     skype_id = fields.Char(string="Skype ID")
-    joining_date = fields.Date("Joining Date")
+    joining_date = fields.Date("Joining Date", tracking=True)
+    blood_group = fields.Selection([
+        ('A+', 'A+'),
+        ('A-', 'A-'),
+        ('B+', 'B+'),
+        ('B-', 'B-'),
+        ('AB+', 'AB+'),
+        ('O+', 'O+'),
+        ('O-', 'O-'),
+    ], default="A+")
