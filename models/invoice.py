9aab81f94c3dbbf08d533f4abf0d44e836a96dc3
niazi12 <niazi.mahrab@northsouth.edu>
2021-12-01 16:20:17 +0600

    Added related partner id to get customer name

--- models/invoice.py
+++ models/invoice.py
@@ -6,12 +6,12 @@ class InvoiceInformation(models.Model):
     _description = 'Invoice Information'
 
     project_id = fields.Many2one('project.project', string='Project name')
-    print('project_id', project_id)
+    partner_id = fields.Many2one('res.partner', string='Customer', related='project_id.partner_id')
 
     @api.onchange("project_id")
     def _onchange_project_name(self):
         account_id = self.env['account.account'].search([], limit=1)
-        partner_id = self.env['project.project'].search([('id', '=', 1)], limit=1)
+        # partner_id = self.env['project.project'].search([('id', '=', 1)], limit=1)
         self.invoice_line_ids = False
         lines = [(5, 0, 0)]
         for line in self.project_id.members_ids:
@@ -22,7 +22,7 @@ class InvoiceInformation(models.Model):
             }
             lines.append((0, 0, val))
         self.invoice_line_ids = lines
-        self.partner_id = partner_id.partner_id
+        # self.partner_id = partner_id.partner_id
 
 
 class InvoiceLineInformation(models.Model):
