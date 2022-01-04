0f47f06fa850a508846f12ac51bd8453736cd9a6
niazi12 <niazi.mahrab@northsouth.edu>
2022-01-04 16:15:22 +0600

    added related department updated

--- __manifest__.py
+++ __manifest__.py
@@ -26,6 +26,7 @@
         'views/dashboard_views.xml',
         'wizard/dept_wise_project_dashboard_wizard.xml',
         'report/project_report.xml',
+        'report/project_report_pdf.xml',
     ],
     'qweb': ['static/src/xml/dashboard.xml'],
     'installable': True,
