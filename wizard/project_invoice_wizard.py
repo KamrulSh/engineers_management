b501efaf1797a7e4875d4446a31c508a67c0485d
Kamrul Islam <shahin.cuetcse@gmail.com>
2022-01-10 15:25:39 +0600

    preview and print project wise invoice report from wizard

--- wizard/project_invoice_wizard.py
+++ wizard/project_invoice_wizard.py
@@ -4,11 +4,41 @@ from odoo import models, fields, api, _
 class ProjectDashboard(models.TransientModel):
     _name = 'project.invoice.wizard'
 
-    project_id = fields.Many2one('project.project', string="Project name", required=True)
+    project_id = fields.Many2one('project.project', string="Project name")
     invoice_id = fields.Many2one('account.move', string="Invoice name")
     invoices = fields.Html("Report invoices")
 
-    def action_print_report(self):
+    def invoices_query(self):
+        select = f"""
+                    SELECT row_number() OVER () AS id,
+                    line.move_id AS line_id,
+                    line.move_name AS invoice_name,
+        			res.name AS customer,
+        			move.invoice_date AS invoice_date,
+                    emp.name AS employee_name,
+        			line.quantity AS quantity,
+                    line.price_unit AS price_unit
+                    FROM account_move_line line
+                    LEFT JOIN account_move move ON move.id = line.move_id
+        			LEFT JOIN res_partner res ON res.id = move.commercial_partner_id
+                    INNER JOIN hr_employee emp ON emp.id = line.project_employee_id"""
+
+        where = f"""
+                    WHERE move.project_id = {self.project_id.id}
+                    """
+        orderby = f"""
+                    ORDER BY invoice_name
+                    """
+
+        if self.project_id:
+            self._cr.execute(select + where + orderby)
+        else:
+            self._cr.execute(select + orderby)
+
+        invoices_data = self._cr.dictfetchall()
+        return invoices_data
+
+    def action_preview_report(self):
         table = """
                     <table border="1" class="o_list_view table table-condensed table-striped o_list_view_ungrouped">
                         <thead>
@@ -34,27 +64,7 @@ class ProjectDashboard(models.TransientModel):
         column_header = ['Invoice ID', 'Invoice Date', 'Customer Name', 'Engineer Name', 'Quantity', 'Price']
         head += thead.format(th="".join(map(th.format, column_header)))
 
-        project_id = self.project_id
-
-        self._cr.execute(f""" 
---             CREATE OR REPLACE VIEW project_invoice_wizard AS ( 
-            SELECT row_number() OVER () AS id,
-            line.move_id AS line_id,
-            line.move_name AS invoice_name,
-			res.name AS customer,
-			move.invoice_date AS invoice_date,
-            emp.name AS employee_name,
-			line.quantity AS quantity,
-            line.price_unit AS price_unit
-            FROM account_move_line line
-            LEFT JOIN account_move move ON move.id = line.move_id
-			LEFT JOIN res_partner res ON res.id = move.commercial_partner_id
-            INNER JOIN hr_employee emp ON emp.id = line.project_employee_id
-            WHERE move.project_id = {project_id.id}
-            ORDER BY invoice_name
---             )
-        """)
-        invoices_data = self._cr.dictfetchall()
+        invoices_data = self.invoices_query()
 
         for i in range(0, len(invoices_data)):
             column_value = [invoices_data[i]['invoice_name'],
@@ -66,6 +76,9 @@ class ProjectDashboard(models.TransientModel):
             body += tr.format("".join(map(td.format, column_value)))
         view_invoices = table.format(thead=head, tbody=body)
         self.write({'invoices': view_invoices})
+
+    def action_print_report(self):
+        invoices_data = self.invoices_query()
 
         data = {
             'form_data': self.read()[0],
